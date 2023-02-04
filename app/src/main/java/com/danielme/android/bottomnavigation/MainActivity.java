b8545bf965a852af58d8fcbbb01c34f673d28c6e
dani <danielme_com@yahoo.com>
2023-02-04 18:48:28 +0100

    integration with Navigation (commented)

--- app/src/main/java/com/danielme/android/bottomnavigation/MainActivity.java
+++ app/src/main/java/com/danielme/android/bottomnavigation/MainActivity.java
@@ -3,9 +3,15 @@ package com.danielme.android.bottomnavigation;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.annotation.DrawableRes;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.danielme.android.bottomnavigation.fragments.PageFragment;
 import com.danielme.android.bottomnavigation.fragments.RecyclerViewFragment;
@@ -23,7 +29,21 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     setupToolbar();
     setupBottomMenu(savedInstanceState);
+    //setupNavigation();
   }
+
+  /*private void setupNavigation() {
+    BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+    NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+    NavController navController = navHostFragment.getNavController();
+    NavigationUI.setupWithNavController(bottomNavigationView,
+            navController);
+
+    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.page_home,
+            R.id.page_fav, R.id.page_list, R.id.page_settings, R.id.page_search)
+            .build();
+    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+  }*/
 
   @Override
   protected void onSaveInstanceState(Bundle outState) {
@@ -54,31 +74,36 @@ public class MainActivity extends AppCompatActivity {
   private boolean onItemSelectedListener(MenuItem item) {
     switch (item.getItemId()) {
       case R.id.page_home:
-        showFragment(PageFragment.newInstance(R.drawable.baseline_home_black_48));
+        showPageFragment(R.drawable.baseline_home_black_48, R.string.bottom_nav_home);
         return true;
       case R.id.page_list:
-        showFragment(new RecyclerViewFragment());
+        showFragment(new RecyclerViewFragment(), R.string.bottom_nav_list);
         return true;
       case R.id.page_fav:
-        showFragment(PageFragment.newInstance(R.drawable.baseline_favorite_black_48));
+        showPageFragment(R.drawable.baseline_favorite_black_48, R.string.bottom_nav_fav);
         return true;
       case R.id.page_search:
-        showFragment(PageFragment.newInstance(R.drawable.baseline_search_black_48));
+        showPageFragment(R.drawable.baseline_search_black_48, R.string.bottom_nav_search);
         return true;
       case R.id.page_settings:
-        showFragment(PageFragment.newInstance(R.drawable.baseline_app_settings_alt_black_48));
+        showPageFragment(R.drawable.baseline_app_settings_alt_black_48, R.string.bottom_nav_settings);
         return true;
       default:
         throw new IllegalArgumentException("item not implemented : " + item.getItemId());
     }
   }
 
-  private void showFragment(Fragment frg) {
+  private void showPageFragment(@DrawableRes int iconId, @StringRes int title) {
+    showFragment(PageFragment.newInstance(iconId), title);
+  }
+
+  private void showFragment(Fragment frg, @StringRes int title) {
     getSupportFragmentManager()
             .beginTransaction()
             .setCustomAnimations(R.anim.bottom_nav_enter, R.anim.bottom_nav_exit)
             .replace(R.id.container, frg)
             .commit();
+    setTitle(title);
   }
 
 }
